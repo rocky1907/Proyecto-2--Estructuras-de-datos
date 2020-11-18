78b25aec044484fe8c29e0614937e84fe4943109
rocky1907 <alemanrodrigo1907@gmail.com>
2020-11-18 14:04:18 -0600

    Update

---
+++ Analizador.py
@@ -0,0 +1,83 @@
+import re
+import hashlib
+diccionarioVariables=dict(nameVoid='void', nameString='string', nameFloat='float', nameInt='int')
+diccionarioPalabraRe=dict(nameIf='if', nameWhile='while')
+diccionarioCaracter=dict(mas='+', menos='-', punComa=';', asterisco='*', coma=',',
+                         diago='/', igual='=', igualacion='==', diferente='!=', mener='<', menIgual='<=', mayIgual='>=',
+                         mayor='>', parentesis=['(',')'], corchetes=['{','}'])
+
+class Analizador:
+    def __init__(self):
+        self.nombre=""
+        self.tipo=""
+        self.identificador=""
+        self.funcion=""
+
+    def setNombre(self, nombre):
+        self.nombre=nombre
+
+    def setIdentificador(self, identificador):
+        self.identificador=identificador
+
+    def setTipo(self, tipo):
+        self.tipo=tipo
+
+    def setFuncion(self, funcion):
+        self.funcion=funcion
+
+    def getNombre(self):
+        return self.nombre
+
+    def getIdentificador(self):
+        return self.identificador
+
+    def getTipo(self):
+        return self.tipo
+
+    def getFuncion(self):
+        return self.funcion
+
+    def mostrar(self):
+        var="Tipo: " + self.tipo + "\n" + "Nombre Variable: " + self.nombre + "\n" + "identificador: " + self.identificador + "Funcion: " + self.funcion
+        return var
+
+    def es_Variable(self, dato):
+        for aux in diccionarioVariables:
+            if diccionarioVariables[aux] == dato:
+                return True
+        return False
+    def es_Palabra(self, dato):
+        for aux in diccionarioPalabraRe:
+            if diccionarioPalabraRe[aux] == dato:
+                return True
+        return False
+    def es_Caracter(self,dato):
+        for aux in diccionarioCaracter:
+            if aux == "corchetes" or aux=="parentesis":
+                if diccionarioCaracter[aux][0]==dato or diccionarioCaracter[aux][1]==dato:
+                    return True
+            if diccionarioCaracter[aux] == dato:
+                return True
+        return False
+    def es_String(self, dato):
+        return dato[0] == -30 and dato[dato.size() - 1] == -99 or dato[0] == 34 and dato[dato.size() - 1] == 34 or dato[0] == 39 and dato[dato.size() - 1] == 39;
+    def es_Numero(self, dato):
+        x=dato.isdigit()
+        return x
+    def funcion_hash(self,dato):
+        pos = hashlib.sha1()
+        pos.update(dato.encode('utf-8'))
+        i = int(pos.hexdigest(),16)
+        return pos.hexdigest()
+
+if __name__ == '__main__':
+    prueba1=Analizador()
+    print(prueba1.es_Variable("string"))
+    print(prueba1.es_Caracter("("))
+    print(prueba1.es_Palabra("while"))
+    print(prueba1.es_String('Hola'))
+    print(prueba1.es_Numero("2"))
+    print(prueba1.funcion_hash("Hola"))
+    '''print(diccionarioPalabra['nameIf'])
+    print(diccionarioVariables['nameInt'])
+    print(diccionarioCaracter['mayor'])'''
--- AnalizadorSemantico.py
+++ AnalizadorSemantico.py
@@ -1,33 +1,58 @@
 import ast
+from distutils.command.config import config
 
 import item as item
-
+import re
 
 class AnalizadorSemantico:
-    # def __init__(self):
-
+    def __init__(self):
+        self.diccionario = {'key':{'int':{'nombre':[],'valor':[],'linea':[]}},'string':{'nombre':[],'valor':[],'linea':[]}
+            ,'void':{'nombre':[],'inicio':[],'fin':[],'linea':[]},'float':{'nombre':[],'valor':[],'linea':[]},
+                            'if':{'nombre':[],'inicio':[],'fin':[],'linea':[]},'while':{'nombre':[],'inicio':[],'fin':[],'linea':[]}}
 
     def _lectura(self):
-        array=[]
-        file= 'Lectura.txt'
-        with open (file) as f_obj:
-            for line in f_obj:
-                array.append(line)
-        return array
+        file='Lectura.txt'
+        try:
+            with open(file,'r',encoding="utf-8") as f_obj:
+                for x in f_obj:
+                    contenido = f_obj.readlines()
+                    self.insertar(contenido)
+        except FileNotFoundError:
+            print("El archivo no existe")
 
-    def leer(self):
-        archivo = open("C:\PROYECTO\Proyecto-2--Estructuras-de-datos", 'r')
-        texto = archivo.read()
-        archivo.close()
-        # convertir a diccionarioimport ast
-        d1 = ast.literal_eval(texto)
-
-        # Los diccionarios tienen estos metodos: keys() para obtener las claves, y values():for item in d1.keys():
-
-        temp = ','.join(d1[item].keys())
-        print(item, ":", temp)
+    def insertar(self, datos):
+        y=0
+        for x in datos:
+            secuencia=r'int'
+            total=re.findall(secuencia, datos[y])
+            if total:
+                self.diccionario['key'][total.__getitem__(y)]
+                print(total)  # busca en una linea la palabra que deseo
+            else:
+                secuencia=r'float'
+                total=re.findall(secuencia, datos[y])
+                if total:
+                    self.diccionario['key'][total.__getitem__(y)]
+                    print(total)  # busca en una linea la palabra que deseo
+                else:
+                    secuencia=r'string'
+                    total=re.findall(secuencia, datos[y])
+                    if total:
+                        self.diccionario['key'][total.__getitem__(y)]
+                        print(total)  # busca en una linea la palabra que deseo
+                        exit()
+            y+=1
+        '''print(len(contenido))  # cuenta el total de lineas
+        secuencia = r'int*'
+        print(re.findall(secuencia,contenido[0]))#busca en una linea la palabra que deseo
+        print(contenido[0])
+        print(contenido)
+        print(len(contenido))#cuenta el total de lineas
+        print('x = x + 5' in contenido)#Busca si existe un elemento en el archivo
+        print(contenido.index('x = x + 5'))#Trae el indice donde esta la palabra buscada
+        print(contenido.count('x ='))#retorna el indice de coincidencias'''
 
 
 if __name__ == '__main__':
-    analizador = AnalizadorSemantico()
-    analizador.leer()
+    prueba = AnalizadorSemantico()
+    prueba._lectura()
